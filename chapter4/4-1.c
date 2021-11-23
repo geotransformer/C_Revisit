c28482de8fe71a9ac5b7866e44d27ce295227300
Fasheng Zhang <faszhang@cisco.com>
2021-11-23 18:05:48 -0500

    chapter 4

---
+++ chapter4/4-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  return 0;
+}
+
+
+/* function returns the position of rightmost occurrence of t in s, or -1 instead */
+int strrindex(char target[], char pattern[])
+{
+  return -1;
+}
+
