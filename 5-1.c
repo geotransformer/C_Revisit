f081143d33a195bdf896c1382ab4d908a0ee7348
Fasheng Zhang <faszhang@cisco.com>
2021-11-25 10:06:52 -0500

    Ch5 pointer and array

---
+++ 5-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    return 0;  
+}
+
+/* As written, getint treats a + or - not followed by a digit as a valid representation of zero. Fix it to push such a character back on the input.*/
+int getint()
+{
+    return 0;
+}
