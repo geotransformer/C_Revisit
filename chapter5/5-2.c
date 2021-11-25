9554eefb4248cff004f2d365629a1beb89c5f70f
Fasheng Zhang <faszhang@cisco.com>
2021-11-25 10:23:15 -0500

    add 5-2.c

---
+++ chapter5/5-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    return 0;  
+}
+
+/* Write getfloat , the floating-point analog of getint . What type does getfloat return as its function value? */
+int getfloat()
+{
+    return 0;
+}
